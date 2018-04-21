5b0f7ea9545a0c3e52f90992b0fd37abe7b34c6b
supernovayo <supernovayoyoyo@gmail.com>
2018-04-21 14:12:55 +0800

    :ant: update

---
+++ README.md
@@ -0,0 +1,25 @@
+# blog
+
+> demo
+
+## 三步走
+
+``` bash
+# 依赖装一波
+npm install
+
+# 会在8080端口跑一个服务
+npm run dev
+
+# 生成生产环境代码
+npm run build
+
+# 生成生产环境代码并生成分析器报告
+npm run build --report
+
+# run unit tests
+npm run unit
+
+# run all tests
+npm test
+```
---
+++ src/pages/Hello.vue
@@ -0,0 +1,45 @@
+
+<template>
+  <div class="hello">
+    <div class="logo">
+      <p>Welcome Use supernovayo</p>
+      <br>
+    </div>
+    <ul>
+      <li><a href="#/index">to index page</a></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+<style scoped lang='less'>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  margin: 0 10px;
+  text-align: center;
+}
+a {
+  color: #42b983;
+}
+.logo{
+  text-align: center;
+  img{
+    width: 125px;
+  }
+}
+</style>
